d2a67289e2b9b66d4cbe1005e42830a0532e297d
dustin-chan-89 <busterdust@gmail.com>
2016-01-07 12:47:23 -0800

    added gem assets and preloaded them

--- js/preload.js
+++ js/preload.js
@@ -2,11 +2,14 @@ var Preload = function(game){};
 
 Preload.prototype = {
 
-	preload: function(){ 
-
+	preload: function(){
+	    this.game.load.image('blue', 'assets/gemBlue.png');
+	    this.game.load.image('green', 'assets/gemGreen.png');
+	    this.game.load.image('red', 'assets/gemRed.png');
+	    this.game.load.image('yellow', 'assets/gemYellow.png');
 	},
 
 	create: function(){
-		this.game.state.start("GameTitle");
+		this.game.state.start("Main");
 	}
-}
+};
