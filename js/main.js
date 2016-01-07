bcf428ef6369dde4cb48abe1b068bce17ee0b75e
dustin-chan-89 <busterdust@gmail.com>
2016-01-07 14:29:08 -0800

    added tileDown function that handles animating the tile to its correct position

--- js/main.js
+++ js/main.js
@@ -27,8 +27,47 @@ Main.prototype = {
 
     var seed = Date.now();
     me.random = new Phaser.RandomDataGenerator([seed]);
-    	
+
   },
+
+  initTiles: function () {
+  	for (var i = 0; i < me.tileGrid.length; i++) {
+  		for (var j = 0; j < me.tileGrid.length; j++) {
+  			var tile = me.addTile(i, j);
+  			me.tileGrid[i][j] = tile;
+  		}
+  	}
+  },
+
+	addTile: function(x, y){
+	 
+	    var me = this;	 
+	    var tileToAdd = me.tileTypes[me.random.integerInRange(0, me.tileTypes.length - 1)]; 
+	 
+	    var tile = me.tiles.create((x * me.tileWidth) + me.tileWidth / 2, 0, tileToAdd);
+	 
+	    me.game.add.tween(tile).to({y:y*me.tileHeight+(me.tileHeight/2)}, 500, Phaser.Easing.Linear.In, true)
+	 
+	    tile.anchor.setTo(0.5, 0.5);
+	 
+	    tile.inputEnabled = true;
+	 
+	    tile.tileType = tileToAdd;
+	 
+	    tile.events.onInputDown.add(me.tileDown, me);
+	 
+	    return tile;
+	 
+	},
+
+	tileDown: function (tile, pointer) {
+		var me = this;
+		if (me.canMove) {
+			me.activeTile1 = tile;
+			me.startPosX = (tile.x - me.tileWidth/2) / me.tileWidth;
+			me.startPosY = (tile.y - me.tileWidth/2) / me.tileHeight;
+		}
+	},
 
 	update: function() {
 
