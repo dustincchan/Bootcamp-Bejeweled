58306fb41f7f2adb2cea548bbd1e4e0d3a423d02
dustin-chan-89 <busterdust@gmail.com>
2016-01-08 00:54:16 -0800

    added algorithm that finds triple or greater matches both horizontally and vertically

--- js/main.js
+++ js/main.js
@@ -42,18 +42,17 @@ Main.prototype = {
 	addTile: function(x, y){
 	 
 	    var me = this;	 
+	    //Randomly selects each tile before adding them to the grid
 	    var tileToAdd = me.tileTypes[me.random.integerInRange(0, me.tileTypes.length - 1)]; 
-	 
+
+	 		//Drops at the correct x position but initializes at the top of the screen
 	    var tile = me.tiles.create((x * me.tileWidth) + me.tileWidth / 2, 0, tileToAdd);
-	 
+	 		
 	    me.game.add.tween(tile).to({y:y*me.tileHeight+(me.tileHeight/2)}, 500, Phaser.Easing.Linear.In, true)
-	 
+	 		
 	    tile.anchor.setTo(0.5, 0.5);
-	 
 	    tile.inputEnabled = true;
-	 
 	    tile.tileType = tileToAdd;
-	 
 	    tile.events.onInputDown.add(me.tileDown, me);
 	 
 	    return tile;
@@ -116,3 +115,103 @@ Main.prototype = {
 	    }
 	 
 	},
+
+	checkMatch: function () {
+		var me = this;
+		var matches = me.getMatches(me.tileGrid);
+
+		if (matches.length > 0) {
+			me.removeTileGroup(matches);
+			me.resetTile();
+			me.fillTile();
+			me.game.time.events.add(500, function () {
+				me.tileUp();
+			});
+			me.game.time.events.add(600, function () {
+				me.checkMatch();
+			});
+		}	else {
+			//Else no match on tile swap, so the tiles animate to reset their position
+			me.swapTiles();
+			me.game.time.events.add(500, function (){
+				me.tileUp();
+				me.canMove = true;
+			});
+		}
+	},
+
+	//Resets active tiles
+	tileUp: function () {
+		var me = this;
+		me.activeTile1 = null;
+		me.activeTile2 = null;
+	},
+
+	getMatches: function (tileGrid) {
+		var matches = [];
+		var groups = [];
+
+		for (var i = 0; i < tileGrid; i++) {
+			var tempArr = tileGrid[i];
+			groups = [];
+			for (var j = 0; j < tempArr.length; j++) {
+				if (j < tempArr.length - 2)
+					if (tileGrid[i][j] && tileGrid[i][j + 1] && tileGrid[i][j + 2]) {
+
+						//Finds a horizontal match
+          	if (tileGrid[i][j].tileType == tileGrid[i][j+1].tileType && tileGrid[i][j+1].tileType == tileGrid[i][j+2].tileType) {
+          		if (groups.length > 0) {
+          			if (groups.indexOf(tileGrid[i][j]) == -1) {
+          				matches.push(groups);
+          				groups = [];
+          			}
+          		}
+          		if (groups.indexOf(tileGrid[i][j]) == -1) {
+          			groups.push(tilegrid[i][j]);
+          		}
+          		if (groups.indexOf(tileGrid[i][j+1]) == -1) {
+          			groups.push(tilegrid[i][j+1]);
+          		}
+          		if (groups.indexOf(tileGrid[i][j+2]) == -1) {
+          			groups.push(tileGrid[i][j+2]);
+          		}
+          	}
+					}
+			}
+			if (groups.length > 0) matches.push(groups);
+		}
+
+		//Now for vertical matches
+		for (j = 0; j < tileGrid.length; j++) {
+			var tempArr = tileGrid[j];
+			groups = [];
+			//Iterate through each gem in a column
+			for (i = 0; i < tempArr.length; i++) {
+				if (i < tempArr.length - 2)
+					if (tileGrid[i][j] && tileGrid[i+1][j] && tileGrid[i+2][j]) {
+						if (tileGrid[i][j].tileType == tileGrid[i+1][j].tileType && tileGrid[i+1][j].tileType == tileGrid[i+2][j].tileType) {
+							if (groups.length > 0) {
+								if (groups.indexOf(tileGrid[i][j]) == -1) {
+									matches.push(groups);
+									groups = [];
+								}
+							}
+							if (groups.indexOf(tileGrid[i][j]) == -1) {
+								groups.push(tileGrid[i][j]);
+							}
+							if (groups.indexOf(tileGrid[i+1][j]) == -1) {
+								groups.push(tileGrid[i+1][j]);
+							}
+							if (groups.indexOf(tileGrid[i+2][j]) == -1) {
+								groups.push(tileGrid[i+2][j]);
+							}
+						}
+					}
+			}
+			if (groups.length > 0) { matches.push(groups) };
+		}
+
+		return matches;
+	},
+
+
