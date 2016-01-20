f9ad264a11396a72a079aa91a36ec1a868fa0ab3
dustin-chan-89 <busterdust@gmail.com>
2016-01-19 19:08:00 -0800

    finalized welcome menu

--- js/gametitle.js
+++ js/gametitle.js
@@ -3,14 +3,20 @@ var GameTitle = function(game){};
 GameTitle.prototype = {
 
 	create: function(){
-
 		this.game.stage.backgroundColor = "#333"
 		var title = game.add.sprite(game.width / 2, 60, "gametitle");
+		title.scale.setTo(0.8, 0.8);
 		title.anchor.set(0.5); 
 		var grid = game.add.sprite(game.width / 2, 130, "gridedition");
+		grid.scale.setTo(0.5, 0.5);
 		grid.anchor.set(0.5);
-		var playButton = game.add.button(game.width / 2, game.height / 2, "playbutton", function(){this.startGame()}.bind(this));
+		var playButton = game.add.button(game.width / 2, game.height / 2 + 50, "playbutton", function(){this.startGame()}.bind(this));
 		playButton.anchor.set(0.5);
+
+		var style = { font: "20px Arial", fill: "#ffffff", align: "center" };
+		var text = game.add.text(game.world.centerX, game.height - 50, "(Click and drag to swap and match logos)", style);
+		text.anchor.set(0.5);
+
 	},
 
 	startGame: function(){
--- js/preload.js
+++ js/preload.js
@@ -10,12 +10,9 @@ Preload.prototype = {
 	    this.game.load.spritesheet('coinflip', 'assets/coinflip.png', 128, 128);
 	    this.game.load.audio('chaching', ['assets/chaching.mp3', 'assets/chaching.ogg']);
 	    this.game.load.audio('bgm', 'assets/money.mp3');
-      this.game.load.image("gametitle", "assets/sprites/gametitle.png");
+      this.game.load.image("gametitle", "assets/sprites/bootcamp.png");
       this.game.load.image("gridedition", "assets/sprites/gridedition.png");
       this.game.load.image("playbutton", "assets/sprites/playbutton.png");
-      this.game.load.image("menubutton", "assets/sprites/menubutton.png");
-      this.game.load.image("resetgame", "assets/sprites/resetgame.png");
-      this.game.load.image("thankyou", "assets/sprites/thankyou.png");
 	},
 
 	create: function(){
