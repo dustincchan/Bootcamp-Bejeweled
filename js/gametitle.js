46c0c37045ff8783df2bf5d7b40ca57caa2d6957
dustin-chan-89 <busterdust@gmail.com>
2016-01-19 17:15:05 -0800

    have working menu

--- js/gametitle.js
+++ js/gametitle.js
@@ -4,6 +4,13 @@ GameTitle.prototype = {
 
 	create: function(){
 
+		this.game.stage.backgroundColor = "#333"
+		var title = game.add.sprite(game.width / 2, 60, "gametitle");
+		title.anchor.set(0.5); 
+		var grid = game.add.sprite(game.width / 2, 130, "gridedition");
+		grid.anchor.set(0.5);
+		var playButton = game.add.button(game.width / 2, game.height / 2, "playbutton", function(){this.startGame()}.bind(this));
+		playButton.anchor.set(0.5);
 	},
 
 	startGame: function(){
--- js/preload.js
+++ js/preload.js
@@ -7,13 +7,18 @@ Preload.prototype = {
 	    this.game.load.image('green', 'assets/dev bootcamp.png');
 	    this.game.load.image('red', 'assets/maker school.png');
 	    this.game.load.image('yellow', 'assets/hack reactor.png');
-	    this.game.load.spritesheet('explosions', 'assets/atomic explosion.png', 96, 96);
 	    this.game.load.spritesheet('coinflip', 'assets/coinflip.png', 128, 128);
 	    this.game.load.audio('chaching', ['assets/chaching.mp3', 'assets/chaching.ogg']);
 	    this.game.load.audio('bgm', 'assets/money.mp3');
+      this.game.load.image("gametitle", "assets/sprites/gametitle.png");
+      this.game.load.image("gridedition", "assets/sprites/gridedition.png");
+      this.game.load.image("playbutton", "assets/sprites/playbutton.png");
+      this.game.load.image("menubutton", "assets/sprites/menubutton.png");
+      this.game.load.image("resetgame", "assets/sprites/resetgame.png");
+      this.game.load.image("thankyou", "assets/sprites/thankyou.png");
 	},
 
 	create: function(){
-		this.game.state.start("Main");
+		this.game.state.start("GameTitle");
 	}
 };
